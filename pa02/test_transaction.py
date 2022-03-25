bdb2d091c72c66bb9e9b03c9851431d9e1d9ac1b
Amanda Cao <64047904+amandacao@users.noreply.github.com>
2022-03-25 01:00:16 -0400

    additional changes to pytest

--- pa02/test_transaction.py
+++ pa02/test_transaction.py
@@ -28,45 +28,39 @@ def small_db(empty_db):
 
 # by amanda
 @pytest.mark.selectall
-def test_selectall(med_db):
-    ''' add a category to db, updates it, and then select all'''
-    cat0 = {'name':'testing_add',
-            'desc':'see if it works',
+def test_selectall(small_db):
+    ''' add a transaction to db, updates it, and then select all'''
+    trans0 = {'item':'testing_add',
+            'amount': 0,
+            'category' : 'testing',
+            'date': '2022-03-25',
+            'description':'see if it works',
             }
-    cats0 = med_db.select_all()
-    assert len(cats0) == len(med_db)+1
+    trans0 = small_db.select_all()
+    assert len(trans0) == len(small_db)+1
     
- # by amanda    
+# by amanda    
 @pytest.mark.select
-def test_select(med_db):
+def test_select(small_db):
     ''' add a category to db, updates it, and then select it'''
-    cat0 = {'name':'testing_add',
-            'desc':'see if it works',
+    trans0 = {'item':'testing_add',
+            'amount': 0,
+            'category' : 'testing',
+            'date': '2022-03-25',
+            'description':'see if it works',
             }
-    rowid = med_db.add(cat0)
-    cat1 = med_db.select_one(rowid)
-    assert cat0['name']==cat1['name']
-    assert cat0['desc']==cat1['desc']
+    rowid = small_db.add(trans0)
+    trans1 = small_db.select_one(rowid)
+    assert trans0['item #']==trans1['item #']
+    assert trans0['description']==trans1['description']
     
 @pytest.mark.summarize
 def test_summarize():
     ''' create new db, add rows, then summarize'''
+    # add rows amount, category, date (yyyymmdd), description
+    result = Transaction(small_db).summarize()
     
-    # create an empty db
-    df = empty_db():
-    
-    # add rows
-    amount, category, date (yyyymmdd), description
-    row1 = {'amount':35,'category':'groceries', 'date': '2022-05-02' , 'description': 'buying stuff'}
-    row2 = {'amount':25,'category':'movies', 'date': '2022-05-02' , 'description': 'buying stuff'}
-    row3 = {'amount':20,'category':'groceries', 'date': '2022-03-02' , 'description': 'buying stuff'}
-    db.add(row1)
-    db.add(row2)
-    db.add(row3)
-    
-    result = db.summarize()
-    
-    assert len(result) = 2
+    assert len(result) == 2
     
     
     
